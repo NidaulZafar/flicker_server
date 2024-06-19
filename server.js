6b0cf8c26efae6c8774ac32d82877355269d03d6
Zafar <n_zafar_007@hotmail.com>
2024-06-19 02:08:22 +0200

    flicker api called

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require("express");
+import express from "express";
+import fetch from "node-fetch";
+
 const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/images", async (req, res) => {
+  console.log("req.query");
+  const { tags } = req.query;
+  const url = `https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1${
+    tags ? `&tags=${tags}` : ""
+  }`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
